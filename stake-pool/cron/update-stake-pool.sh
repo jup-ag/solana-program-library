3b62a6ff9ddeb7f2057cd0ef5bef92acc9535047
Bogdan Shumygora <b.shumygora@everstake.one>
2022-08-02 10:50:46 +0300

    Add cron-related things to the repo

---
+++ stake-pool/cron/update-stake-pool.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/stakepool/solana-program-library/target/release/spl-stake-pool update GUAMR8ciiaijraJeLDEDrFVaueLm9YzWWY9R7CBPL9rA >> /home/stakepool/cron/update-stake-pool.log "$@" > /home/stakepool/cron/update-stake-pool.log 2> /home/stakepool/cron/update-stake-pool.log
